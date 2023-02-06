12dd12848cf18a463b5ad39295ae16da4c2a57ba
vatsalsharma <vatsalsharma4321@gmail.com>
2023-02-07 04:09:49 +0530

    Day 5 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_1st/pages/login_page.dart';
+import 'package:flutter_1st/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
-import 'pages/login_page.dart';
 
 void main(List<String> args) {
-  runApp(Myapp());
+  runApp(const Myapp());
 }
 
 class Myapp extends StatelessWidget {
-  const Myapp({ Key? key }) : super(key: key);
+  const Myapp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: GoogleFonts.lato().fontFamily
-      ),
-      
+          primarySwatch: Colors.blue,
+          fontFamily: GoogleFonts.lato().fontFamily),
+      debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/login",
+      initialRoute: "/",
       routes: {
-        "/":(context) => HomePage(),
-        "/home":(context) => HomePage(),
-        "/login":(context) => LoginPage()
+        "/": (context) => const LoginPage(),
+        MyRoutes.homeRoute: (context) => const HomePage(),
+        MyRoutes.loginRoute: (context) => const LoginPage()
       },
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
-    final int days= 1;
-    final String name= "Vatsal";
+  const HomePage({Key? key}) : super(key: key);
+  final int days = 1;
+  final String name = "Vatsal";
   @override
   Widget build(BuildContext context) {
-     
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: const Text("Catalog App"),
       ),
       body: Center(
         child: Container(
           child: Text("Welcome to the day $days of the App by $name"),
         ),
       ),
-      drawer: Drawer(),
+      drawer: const Drawer(),
     );
   }
 }
